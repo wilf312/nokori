9ddb9a97878001124791e80179e1fbc684dfadef
okada genya <g.okada.wilf+github@gmail.com>
2025-01-18 23:29:07 +0900

    setting: deno startで指定したPORTで起動できるように調整

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
+const port = Number.parseInt(Deno.env.get("PORT") || "13333");
+
 export default defineConfig({
   plugins: [tailwind()],
+  server: {
+    port: port
+  }
 });
